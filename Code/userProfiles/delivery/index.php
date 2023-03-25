435ee95756bb4686499216ff67bb3089e08ac9c7
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2023-03-26 01:59:21 +0600

    navbar changes

---
+++ Code/userProfiles/delivery/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Products Page</title>
+  <link href="../../boxicons-2.1.4/css/boxicons.min.css" rel="stylesheet" />
+  <link href="../../css/bootstrap.min.css" rel="stylesheet" />
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
+  <script defer src="script.js"></script>
+</head>
+<body>
+<?php
+    require 'navbar.php';
+    Navbar();
+    ?>
+  <section>
+    hi
+  </section>
+</body>
+</html>
