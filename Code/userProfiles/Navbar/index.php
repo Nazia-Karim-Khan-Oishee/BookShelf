3a027bfa05b7a22ff8fe9fd4ff56498dc84e4a0e
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2023-03-26 03:23:41 +0600

    changes in navbar

--- Code/userProfiles/Navbar/index.php
+++ Code/userProfiles/Navbar/index.php
@@ -84,9 +84,6 @@
 </button>
 <div class="collapse navbar-collapse" id="navbarNav">
   <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" aria-current="page" href="#">Home</a>
-    </li>
     <li class="nav-item d-lg-none">
       <a class="nav-link">Log Out</a>
     </li>
