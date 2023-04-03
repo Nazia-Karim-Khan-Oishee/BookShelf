d8255989d3e9f0e8390aaa92658622ca27a1ff78
Sadnam Sakib <sadnamsakib@iut-dhaka.edu>
2023-04-03 16:51:34 +0600

    customer can now order books

--- Code/userProfiles/customer/Book/fetch-areas.php
+++ Code/userProfiles/customer/Book/fetch-areas.php
@@ -1,25 +1,35 @@
 <?php
 include '../../../Database/Config.php';
-
-// Check if the district ID is set
+// Check if district is set
 if (isset($_POST['district'])) {
-    // Retrieve the district ID from the POST data
-    $district = $_POST['district'];
 
-    // Query to fetch the areas that belong to the selected district
-    $sql = "SELECT * FROM location WHERE district_id = $district";
+  // Escape special characters in district
+  $district = mysqli_real_escape_string($Conn, $_POST['district']);
 
-    // Execute the query and fetch the results
-    $result = mysqli_query($Conn, $sql);
+  // Query to get all areas under the selected district
+  $sql = "SELECT DISTINCT area FROM location WHERE district = '$district'";
+  $result = mysqli_query($Conn, $sql);
+
+  // Check if there are any areas found
+  if (mysqli_num_rows($result) > 0) {
+    // Create an array to hold the areas
     $areas = array();
+    // Loop through all areas and add them to the array
     while ($row = mysqli_fetch_assoc($result)) {
-        $areas[] = $row;
+      $areas[] = $row['area'];
     }
-
     // Return the areas as a JSON-encoded array
     echo json_encode($areas);
+  } else {
+    // If no areas found, display a message
+    echo json_encode("No areas found");
+  }
+
+} else {
+  // If district is not set, display a message
+  echo json_encode(array('message' => 'Please select a district first'));
 }
 
-// Close the database connection
+// Close the database Connection
 mysqli_close($Conn);
 ?>
